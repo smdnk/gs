18567e8d1dc76eab97ad0470fc9170b5989b4cfa
liulongfei <w.liulongfei@gmail.com>
2024-03-05 22:43:45 +0800

    go-redis操做redis

--- go-redis/main/main.go
+++ go-redis/main/main.go
@@ -1,14 +1,19 @@
 package main
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+)
 
 func main() {
 
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "192.168.0.6:6379",
 		Password: "", // 没有密码，默认值
 		DB:       0,  // 默认DB 0
 	})
-
-	rdb.Conn()
+	ctx := context.Background()
+	s := rdb.Ping(ctx).String()
+	val, _ := rdb.Get(ctx, "11").Result()
+	print(val, s)
 }
